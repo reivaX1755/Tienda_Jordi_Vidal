6d437c6f536d26a12ad9f39450e1b371ab0b264c
Jordi Vidal <163132865+reivaX1755@users.noreply.github.com>
2024-11-25 17:38:48 +0100

    the #5 code formated

--- Tienda_Jordi_Vidal/src/dao/DaoImplXml.java
+++ Tienda_Jordi_Vidal/src/dao/DaoImplXml.java
@@ -1,6 +1,5 @@
 package dao;
 
-
 import java.util.ArrayList;
 
 import javax.xml.parsers.SAXParser;
@@ -11,12 +10,13 @@ import dao.xml.DomWriter;
 import model.Employee;
 import model.Product;
 
-public class DaoImplXml implements Dao{
+public class DaoImplXml implements Dao {
 
 	public ArrayList<Product> inventory;
+
 	@Override
 	public void connect() {
-		
+
 	}
 
 	@Override
@@ -26,37 +26,37 @@ public class DaoImplXml implements Dao{
 
 	@Override
 	public void disconnect() {
-		
+
 	}
 
 	@Override
 	public ArrayList<Product> getInventory() {
 		inventory = new ArrayList<Product>();
 		SAXParserFactory factory = SAXParserFactory.newInstance();
-        SAXParser saxParser;
-        
+		SAXParser saxParser;
+
 		try {
 			saxParser = factory.newSAXParser();
-            SaxReader saxReader = new SaxReader();
-            saxParser.parse("src/Files/inputInventory.xml", saxReader);
-            inventory = saxReader.getProducts();
-            return inventory;
-        } catch (Exception e) {
-        	e.printStackTrace(); //Metodo para saber que falla exactamente
-        	return null;
-        }
+			SaxReader saxReader = new SaxReader();
+			saxParser.parse("src/Files/inputInventory.xml", saxReader);
+			inventory = saxReader.getProducts();
+			return inventory;
+		} catch (Exception e) {
+			e.printStackTrace(); // Metodo para saber que falla exactamente
+			return null;
+		}
 	}
 
 	@Override
 	public boolean writeInventory(ArrayList<Product> inventory) {
 		DomWriter domWriter = new DomWriter();
 		boolean created = domWriter.generateDocument(inventory);
-	    if(created) {
-	    	return true;
-	    }else {
-	    	return false;
-	    }
-	    
+		if (created) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 
 }
