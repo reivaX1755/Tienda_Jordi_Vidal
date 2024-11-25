0a236ce69428c93aca4ad54f215ee093c7dca8da
Jordi Vidal <163132865+reivaX1755@users.noreply.github.com>
2024-11-25 17:35:56 +0100

    the #9 code formated

--- Tienda_Jordi_Vidal/src/dao/xml/SaxReader.java
+++ Tienda_Jordi_Vidal/src/dao/xml/SaxReader.java
@@ -10,52 +10,53 @@ import model.Amount;
 import model.Product;
 
 public class SaxReader extends DefaultHandler {
-    ArrayList<Product> products;
-    Product product;
-    String value;
-    String parsedElement;
+	ArrayList<Product> products;
+	Product product;
+	String value;
+	String parsedElement;
 
-    public ArrayList<Product> getProducts() {
-        return products;
-    }
+	public ArrayList<Product> getProducts() {
+		return products;
+	}
 
-    @Override
-    public void startDocument() throws SAXException {
-        this.products = new ArrayList<>();
-    }
+	@Override
+	public void startDocument() throws SAXException {
+		this.products = new ArrayList<>();
+	}
 
-    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-        switch (qName) {
-            case "product":
-                this.product = new Product(attributes.getValue("name") != null ? attributes.getValue("name") : "empty", new Amount(0.0), true, 0);
-                break;
-            case "wholesalerPrice":
-                break;
-            case "stock":
-                break;
-        }
-        this.parsedElement = qName;
-    }
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		switch (qName) {
+		case "product":
+			this.product = new Product(attributes.getValue("name") != null ? attributes.getValue("name") : "empty",
+					new Amount(0.0), true, 0);
+			break;
+		case "wholesalerPrice":
+			break;
+		case "stock":
+			break;
+		}
+		this.parsedElement = qName;
+	}
 
-    public void characters(char[] ch, int start, int length) throws SAXException {
-        value = new String(ch, start, length);
-        switch (parsedElement) {
-            case "product":
-                break;
-            case "wholesalerPrice":
-                this.product.setWholesalerPrice(new Amount(Double.valueOf(value)));
-                this.product.setPublicPrice(new Amount(this.product.getWholesalerPrice().getValue() * 2));
-                break;
-            case "stock":
-                this.product.setStock(Integer.valueOf(value));
-                break;
-        }
-    }
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		value = new String(ch, start, length);
+		switch (parsedElement) {
+		case "product":
+			break;
+		case "wholesalerPrice":
+			this.product.setWholesalerPrice(new Amount(Double.valueOf(value)));
+			this.product.setPublicPrice(new Amount(this.product.getWholesalerPrice().getValue() * 2));
+			break;
+		case "stock":
+			this.product.setStock(Integer.valueOf(value));
+			break;
+		}
+	}
 
-    public void endElement(String uri, String localName, String qName) throws SAXException {
-        if (qName.equals("product")) {
-            this.products.add(product);
-        }
-        this.parsedElement = "";
-    }
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		if (qName.equals("product")) {
+			this.products.add(product);
+		}
+		this.parsedElement = "";
+	}
 }
